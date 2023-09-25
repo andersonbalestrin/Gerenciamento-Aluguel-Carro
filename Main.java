46ae4eb567cdb2c5a1212d87bb73f9df27cddde4
andersonbalestrin <91353932+andersonbalestrin@users.noreply.github.com>
2023-09-25 20:48:10 -0300

    Create Main.java
    teste

---
+++ Main.java
@@ -0,0 +1,20 @@
+package gerenciamentoaluguelcarro;
+
+import gerenciamentoaluguelcarro.Carro;
+import gerenciamentoaluguelcarro.Aluguel;
+
+public class Main {
+    public static void main(String[] args) {
+        Usuario usuario = new Usuario("Rogério", "123.123.123-11", "(11)1111-1111");
+        Carro carro = new Carro("HB20", "Hyundai", 5.0);
+        Aluguel aluguel = new Aluguel(usuario, carro, 1);
+
+        aluguel.iniciarAluguel(100);
+        aluguel.fecharLocacao(200);
+
+        String resumo = aluguel.mostrarResumoLocacao();
+        System.out.println(resumo);
+    }
+}
+    
+}
